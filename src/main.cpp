80566e37d1541d0cdc18762c557c1e55f5a79bf0
Paradisum <2513209473@qq.com>
2023-09-29 19:48:37 +0800

    test

--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,16 @@ using namespace std;
 
 int main (){
     cout<<"TEST"<<endl;
-    tb_char_t path1[1024]= "/private/tmp/xxx";
+    tb_char_t path1[1024]= "/private/tmp/.lolly/chdir";
     tb_char_t  path2[1024];
 
     // 创建目录
-    tb_directory_create("/private/tmp/xxx");
+    tb_directory_create("/private/tmp/.lolly");
+    tb_directory_create("/private/tmp/.lolly/chdir");
     cout<<"[DEBUG]:"<<path1<<endl;
 
     // 设置为/private/tmp/xxx
-    tb_directory_current_set("/private/tmp/xxx");
+    tb_directory_current_set("/private/tmp/.lolly/chdir");
     tb_directory_current(path2, 1024);
     cout<<"[DEBUG]:"<<path2<<endl;
 
