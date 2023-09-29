7db16c9d24c602e74d2c3aeaace4deb4069b5f08
Paradisum <2513209473@qq.com>
2023-09-29 19:16:55 +0800

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
+#include <cassert>
+#include <string>
 #include "tbox/tbox.h"
 using namespace std;
 
 int main (){
     cout<<"TEST"<<endl;
-    tb_char_t path[1024];
-    tb_directory_current(path, 1024);
-    cout<<"[DEBUG]:"<<path<<endl;
-    tb_directory_create("/tmp/xxx/");
-    tb_directory_current_set("/tmp/xxx/");
+    tb_char_t path1[1024]= "/private/tmp/xxx";
+    tb_char_t  path2[1024];
 
-    tb_directory_current_set(path);
-    tb_directory_current(path, 1024);
-    cout<<"[DEBUG]:"<<path<<endl;
+    // 创建目录
+    tb_directory_create("/private/tmp/xxx");
+    cout<<"[DEBUG]:"<<path1<<endl;
+
+    // 设置为/private/tmp/xxx
+    tb_directory_current_set("/private/tmp/xxx");
+    tb_directory_current(path2, 1024);
+    cout<<"[DEBUG]:"<<path2<<endl;
+
+    assert(string(path1) == string(path2));
 }
